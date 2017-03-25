6520de32085be727cbe81931418ca6cc1b1f0784
Barış Elçiseven <baris.elciseven@gmail.com>
2017-03-25 22:42:06 +0300

    combobox eklendi.

--- WindowsFormsApplication1/Form3.Designer.cs
+++ WindowsFormsApplication1/Form3.Designer.cs
@@ -42,11 +42,12 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(333, 30);
+            this.button1.Location = new System.Drawing.Point(251, 22);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 36);
             this.button1.TabIndex = 5;
@@ -57,16 +58,16 @@
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(11, 38);
+            this.listBox1.Location = new System.Drawing.Point(2, 73);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(106, 95);
+            this.listBox1.Size = new System.Drawing.Size(75, 30);
             this.listBox1.TabIndex = 1;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(45, 22);
+            this.label1.Location = new System.Drawing.Point(12, 22);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(42, 13);
             this.label1.TabIndex = 2;
@@ -75,7 +76,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(162, 147);
+            this.label4.Location = new System.Drawing.Point(95, 139);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(50, 13);
             this.label4.TabIndex = 20;
@@ -84,7 +85,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(162, 118);
+            this.label5.Location = new System.Drawing.Point(95, 110);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(50, 13);
             this.label5.TabIndex = 19;
@@ -92,14 +93,14 @@
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(253, 111);
+            this.textBox4.Location = new System.Drawing.Point(186, 103);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(59, 20);
             this.textBox4.TabIndex = 3;
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(253, 140);
+            this.textBox5.Location = new System.Drawing.Point(186, 132);
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(59, 20);
             this.textBox5.TabIndex = 4;
@@ -108,7 +109,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(162, 88);
+            this.label3.Location = new System.Drawing.Point(95, 80);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(63, 13);
             this.label3.TabIndex = 16;
@@ -117,7 +118,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(162, 59);
+            this.label2.Location = new System.Drawing.Point(95, 51);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(49, 13);
             this.label2.TabIndex = 15;
@@ -126,7 +127,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(162, 33);
+            this.label6.Location = new System.Drawing.Point(95, 25);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(86, 13);
             this.label6.TabIndex = 14;
@@ -134,28 +135,28 @@
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(253, 81);
+            this.textBox3.Location = new System.Drawing.Point(186, 73);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(59, 20);
             this.textBox3.TabIndex = 2;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(253, 56);
+            this.textBox2.Location = new System.Drawing.Point(186, 48);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(59, 20);
             this.textBox2.TabIndex = 1;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(253, 30);
+            this.textBox1.Location = new System.Drawing.Point(186, 22);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(59, 20);
             this.textBox1.TabIndex = 0;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(12, 139);
+            this.button2.Location = new System.Drawing.Point(2, 116);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 36);
             this.button2.TabIndex = 6;
@@ -163,11 +164,21 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(2, 43);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(75, 21);
+            this.comboBox1.TabIndex = 21;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(435, 189);
+            this.ClientSize = new System.Drawing.Size(330, 184);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label5);
@@ -206,5 +217,6 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
--- WindowsFormsApplication1/Form3.cs
+++ WindowsFormsApplication1/Form3.cs
@@ -31,7 +31,8 @@ namespace WindowsFormsApplication1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int index = listBox1.SelectedIndex;
+            //int index = listBox1.SelectedIndex;
+            int index = comboBox1.SelectedIndex;
             Form1.gemiler.ElementAt(index).emniyet_alani = (Form1.katSayi * Convert.ToInt32(textBox1.Text));
             Form1.gemiler.ElementAt(index).hiz = Convert.ToInt32(textBox2.Text);
             if (Convert.ToInt32(textBox3.Text) > 0)
@@ -51,10 +52,12 @@ namespace WindowsFormsApplication1
 
         private void gemileriGetir()
         {
-            listBox1.Items.Clear();
+            //listBox1.Items.Clear();
+            comboBox1.Items.Clear();
             for (int i = 0; i < Form1.gemiler.Count; i++)
             {
-                listBox1.Items.Add("Gemi "+Form1.gemiler.IndexOf(Form1.gemiler.ElementAt(i)));
+                //listBox1.Items.Add("Gemi "+Form1.gemiler.IndexOf(Form1.gemiler.ElementAt(i)));
+                comboBox1.Items.Add("Gemi " + Form1.gemiler.IndexOf(Form1.gemiler.ElementAt(i)));
             }
         }
 
@@ -78,12 +81,13 @@ namespace WindowsFormsApplication1
       
         private void button2_Click(object sender, EventArgs e)
         {
-            if (listBox1.SelectedIndex!=0)
+            if (comboBox1.SelectedIndex!=0)
             {
                 DialogResult result = MessageBox.Show("Gemiyi Silmek Istediginizden Emin Misiniz ??", "Onaylama", MessageBoxButtons.YesNoCancel);
                 if (result == DialogResult.Yes)
                 {
-                    Form1.gemiler.RemoveAt(listBox1.SelectedIndex);
+                    //Form1.gemiler.RemoveAt(listBox1.SelectedIndex);
+                    Form1.gemiler.RemoveAt(comboBox1.SelectedIndex);
                     gemileriGetir();
                     f1.Yenile();
                 }
@@ -91,6 +95,23 @@ namespace WindowsFormsApplication1
             else
             {
                 MessageBox.Show("Kendi Gemimiz Silinemez");
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int index = comboBox1.SelectedIndex;
+            if (index >= 0 && Form1.gemiler.Count() > index)
+            {
+                textBox1.Text = Form1.gemiler.ElementAt(index).emniyet_alani / Form1.katSayi + "";
+                if (index >= 1)
+                    textBox1.ReadOnly = true;
+                else
+                    textBox1.ReadOnly = false;
+                textBox2.Text = Form1.gemiler.ElementAt(index).hiz + "";
+                textBox3.Text = Math.Abs(Form1.gemiler.ElementAt(index).rota) + "";
+                textBox4.Text = Form1.gemiler.ElementAt(index).merkez.X + "";
+                textBox5.Text = Form1.gemiler.ElementAt(index).merkez.Y + "";
             }
         }
     }
